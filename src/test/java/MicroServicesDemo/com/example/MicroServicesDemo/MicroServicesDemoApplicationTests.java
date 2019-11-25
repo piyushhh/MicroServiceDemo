a99f71959d87efde82fa7cbe30ff52fce72649b6
piyushhh <piyush2oct@gmail.com>
2019-11-25 23:41:04 +0530

    first commit

---
+++ src/test/java/MicroServicesDemo/com/example/MicroServicesDemo/MicroServicesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package MicroServicesDemo.com.example.MicroServicesDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
